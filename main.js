5b1839d644a0a7e220ce57a21a958e8107e14e6c
Asiyafathim <asiyafathim767@gmail.com>
2024-04-14 14:56:32 +0530

    button changes

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    createBoard(32);
-    console.log("hi")
+let color = "black"
+document.addEventListener("DOMContentLoaded", function(){
+    createBoard(16);
+
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click",function(){
+        let size = getSize();
+        createBoard(size);
+    })
 })
 
-function createBoard(size) {
+function createBoard(size){
     let board = document.querySelector(".board");
 
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -11,9 +17,42 @@ function createBoard(size) {
 
     let numDivs = size * size;
 
-    for (let i = 0; i < numDivs; i++) {
+    for (let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
+        div.addEventListener("mouseover",colorDiv)
         board.insertAdjacentElement("beforeEnd", div);
     }
+}
+
+function getSize(){
+    let input = prompt("type the number of squares you require");
+    let message = document.querySelector("#message");
+    if(input == ""){
+        message.innerHTML = "Please provide a number";
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML = "Provide a number between 1 and 100"
+    }
+    else{
+        message.innerHTML = "Now lets play!"
+        return input;
+    }
+}
+
+function colorDiv(){
+    if(color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else{
+        this.style.backgroundColor = 'Black'
+    }
+}
+
+function setColor(colorChoice){
+    color = colorChoice
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = "white")
 }
